5e264b6ce8e0bc43a16d851d349c6050d3fd7fe7
ohisamaTrb <106422847+ohisamaTrb@users.noreply.github.com>
2022-10-12 11:56:11 +0900

    roll中にボタン/シェイクが反応しないように変更

--- main.ts
+++ main.ts
@@ -6,27 +6,35 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(max)
 })
 input.onGesture(Gesture.Shake, function () {
-    showResult()
+    if (roll == false) {
+        showResult()
+    }
 })
 input.onButtonPressed(Button.B, function () {
     max += 1
     basic.showNumber(max)
 })
 function showResult () {
+    roll = true
     random = randint(1, max)
     for (let image of animation) {
         image.showImage(0)
     }
     basic.showNumber(random)
+    roll = false
 }
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    showResult()
+    if (roll == false) {
+        showResult()
+    }
 })
 let animation: Image[] = []
+let roll = false
 let max = 0
 let random = 0
 random = 0
 max = 6
+roll = false
 animation = [
 images.createImage(`
     . . . . .
